185f2a561d2bd3595573102607c5ea08925a64f1
xprojectdevelopers <xprojectdevelopers2025@gmail.com>
2025-10-25 03:27:54 +0800

    23

--- Capstone/obj/Debug/net8.0-windows/Capstone.AssemblyInfo.cs
+++ Capstone/obj/Debug/net8.0-windows/Capstone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e0418ec1d391c4717994283043c45e5f4c7a49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0befca0cb57a44a2e7182a40afc4be825a917197")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
