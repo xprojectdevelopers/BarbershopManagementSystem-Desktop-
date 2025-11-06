87ae0117a1397f7610fe92bb51b9743ab6cb6ff9
xprojectdevelopers <xprojectdevelopers2025@gmail.com>
2025-11-06 22:57:31 +0800

    Image is not done

--- Capstone/obj/Debug/net8.0-windows/Capstone.AssemblyInfo.cs
+++ Capstone/obj/Debug/net8.0-windows/Capstone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d221bd7f75ca81bebd63852ce34446b95d2180")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8099dc00b0eb6e36348744c4803eca6504dc6fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Capstone/obj/Debug/net8.0-windows/ListSubscribers.g.i.cs
+++ Capstone/obj/Debug/net8.0-windows/ListSubscribers.g.i.cs
@@ -84,7 +84,7 @@ namespace Capstone {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Capstone;component/listsubscribers.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Capstone;V1.0.0.0;component/listsubscribers.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\ListSubscribers.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
